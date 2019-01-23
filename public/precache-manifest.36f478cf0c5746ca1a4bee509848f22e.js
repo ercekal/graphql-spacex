6ca549b134895aa810f64f62d15dd249c7e79013
Erce Kalabalikoglu <ercekalabalikoglu@ErceKaloglusMBP.localdomain>
2019-01-23 16:40:45 +0000

    heroku deploy

---
+++ public/precache-manifest.36f478cf0c5746ca1a4bee509848f22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96bf5096374466c231101845e051b46",
+    "url": "/static/media/logo.e96bf509.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c6f45649fdf1e4780b7",
+    "url": "/static/js/main.3c6f4564.chunk.js"
+  },
+  {
+    "revision": "13d91e3e96234f9dc88a",
+    "url": "/static/js/1.13d91e3e.chunk.js"
+  },
+  {
+    "revision": "3c6f45649fdf1e4780b7",
+    "url": "/static/css/main.740717cd.chunk.css"
+  },
+  {
+    "revision": "1b82fc5b08d2bc4c739c3becbe7dcb19",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,59 +1,25 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
-
+const cors = require('cors')
+const path = require('path')
 const app = express()
-const { ApolloServer, gql } = require('apollo-server-express');
-const PORT = process.env.PORT || 5000
 
-//mongoose 
-const mongoose = require('mongoose')
-require('dotenv').config({ path: 'variables.env' })
+app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useCreateIndex: true
-	})
-	.then(() => console.log('db connected'))
-	.catch(err => console.log(err))
-
-// const typeDefs = gql`
-//   type Blog {
-//     title: String!
-// 		author: String
-// 		body: String
-// 		date: String
-//   }
-// 	type User {
-//     username: String!
-//     password: String!
-//     email: String!
-// 		date: String
-//   }
-// 	type Query {
-// 		showAllBlogs: [Blog]
-// 	}
-// `;
-
-
-// const resolvers = {
-//   Query: () => {}
-// };
-
-// const server = new ApolloServer({ 
-// 	typeDefs, 
-// 	resolvers,
-// 	context: {
-// 		Blog, 
-// 		User
-// 	}
-// });
-
-// server.applyMiddleware({ app });
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
 })
